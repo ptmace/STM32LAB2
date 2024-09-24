b5e3345574d82e5beafe36b1e5058c0c4654dd0f
ptmace <anh.phambk107@hcmut.edu.vn>
2024-09-25 01:40:13 +0700

    Update for Ex3

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -225,12 +225,18 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int counter = 100;
+int counter1 = 50;
+int counter2 = 100;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	counter--;
-	if(counter <= 0){
-		counter = 100;
+	counter1--;
+	if(counter1 <= 0){
+		counter1 = 50;
 		exercise3_run();
+		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+	}
+	counter2--;
+	if(counter2 <= 0){
+		counter2 = 100;
 		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	}
 }
